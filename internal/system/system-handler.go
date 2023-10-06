5213b7de9b2ba7999b686813adc419f41e3e18fa
James Cathcart <cathcart.jm@gmail.com>
2023-10-06 16:54:52 -0600

    added health check handler func

---
+++ internal/system/system-handler.go
@@ -0,0 +1,29 @@
+package system
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func HealthCheck(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method != http.MethodGet {
+		http.Error(w, `method not allowed`, http.StatusMethodNotAllowed)
+		return
+	}
+
+	data := struct {
+		Message string `json:message`
+	}{
+		Message: `weather server is up`,
+	}
+
+	jsonBytes, err := json.Marshal(data)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set(`Content-Type`, `application/json`)
+	w.Write(jsonBytes)
+}
