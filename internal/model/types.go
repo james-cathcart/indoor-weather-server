7afe132385eed2dc196f13e806eb8101e7388f84
James Cathcart <cathcart.jm@gmail.com>
2023-10-09 18:25:42 -0600

    added underscore to timestamp fieldname

--- internal/model/types.go
+++ internal/model/types.go
@@ -1,7 +1,7 @@
 package model
 
 type WeatherRecord struct {
-	Timestamp   string  `json:"timestamp"`
+	Timestamp   string  `json:"_timestamp"`
 	Humidity    int     `json:"humidity"`
 	Temperature float64 `json:"temperature"`
 	Pressure    float64 `json:"pressure"`
