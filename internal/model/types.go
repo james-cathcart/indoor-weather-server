493793b5022faeccb8ee7fb05c63adc8689ff5e1
James Cathcart <cathcart.jm@gmail.com>
2023-10-11 08:42:18 -0600

    added fahrenheit field

--- internal/model/types.go
+++ internal/model/types.go
@@ -1,9 +1,10 @@
 package model
 
 type WeatherRecord struct {
-	Timestamp   string  `json:"_timestamp"`
-	Humidity    int     `json:"humidity"`
-	Temperature float64 `json:"temperature"`
-	Pressure    float64 `json:"pressure"`
-	Location    string  `json:"location"`
+	Timestamp    string  `json:"_timestamp"`
+	Humidity     int     `json:"humidity"`
+	Temperature  float64 `json:"temperature"`
+	TemperatureF float64 `json:"temperature_f"`
+	Pressure     float64 `json:"pressure"`
+	Location     string  `json:"location"`
 }
