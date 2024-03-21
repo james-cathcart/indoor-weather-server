1fc46e1717773e1c9d8668186416012beea1a8d4
James Cathcart <cathcart.jm@gmail.com>
2024-03-21 01:55:30 -0600

    added cardinal direction field to model

--- internal/model/types.go
+++ internal/model/types.go
@@ -1,10 +1,11 @@
 package model
 
 type WeatherRecord struct {
-	Timestamp    string  `json:"_timestamp"`
-	Humidity     int     `json:"humidity"`
-	Temperature  float64 `json:"temperature"`
-	TemperatureF float64 `json:"temperature_f"`
-	Pressure     float64 `json:"pressure"`
-	Location     string  `json:"location"`
+	Timestamp         string  `json:"_timestamp"`
+	CardinalDirection string  `json:"cardinal_direction"`
+	Humidity          int     `json:"humidity"`
+	Temperature       float64 `json:"temperature"`
+	TemperatureF      float64 `json:"temperature_f"`
+	Pressure          float64 `json:"pressure"`
+	Location          string  `json:"location"`
 }
