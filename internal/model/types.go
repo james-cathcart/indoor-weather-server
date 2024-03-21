cee81ed3d331805aad03151049f567484c64c5f6
James Cathcart <cathcart.jm@gmail.com>
2024-03-21 02:23:41 -0600

    added floor field to model

--- internal/model/types.go
+++ internal/model/types.go
@@ -2,10 +2,11 @@ package model
 
 type WeatherRecord struct {
 	Timestamp         string  `json:"_timestamp"`
-	CardinalDirection string  `json:"cardinal_direction"`
 	Humidity          int     `json:"humidity"`
 	Temperature       float64 `json:"temperature"`
 	TemperatureF      float64 `json:"temperature_f"`
 	Pressure          float64 `json:"pressure"`
 	Location          string  `json:"location"`
+	CardinalDirection string  `json:"cardinal_direction"`
+	Floor             string  `json:"floor"`
 }
